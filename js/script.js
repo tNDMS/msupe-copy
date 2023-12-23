83209a03b963d1978b3bfcae2ddc7a0a849d1e31
tNDMS <110533976+tNDMS@users.noreply.github.com>
2023-12-23 19:21:26 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict"
+const iconMenu = document.querySelector('.icon-menu');
+if (iconMenu) {
+	const menuBody = document.querySelector('.menu__body');
+	const logoBurger = document.querySelector('.header__logo');
+	iconMenu.addEventListener("click", function (e) {
+		document.body.classList.toggle('_lock');
+		iconMenu.classList.toggle('_active');
+		menuBody.classList.toggle('_active');
+		logoBurger.classList.toggle('_active');
+	}
+	)
+}
+
+
