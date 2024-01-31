198a7596aacf36d9f9a24e28d8c2a9829da55b53
tNDMS-mielsen144@mail.ru <mielsen144@mail.ru>
2024-01-31 10:55:30 +0300

    31.01 yandex maps, burger

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 "use strict"
-const iconMenu = document.querySelector('.icon-menu');
+const iconMenu = document.querySelector('.menu__icon');
+console.log(iconMenu)
 if (iconMenu) {
 	const menuBody = document.querySelector('.menu__body');
-	const logoBurger = document.querySelector('.header__logo');
+	// const logoBurger = document.querySelector('.header__logo');
 	iconMenu.addEventListener("click", function (e) {
-		document.body.classList.toggle('_lock');
+		// document.body.classList.toggle('_lock');
 		iconMenu.classList.toggle('_active');
 		menuBody.classList.toggle('_active');
-		logoBurger.classList.toggle('_active');
+		// logoBurger.classList.toggle('_active');
 	}
 	)
 }
